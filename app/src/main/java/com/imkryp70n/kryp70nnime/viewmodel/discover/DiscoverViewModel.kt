8ad384f926b8164f4b5edeba39f400169fec0044
kryp70n <joemusicamv12@gmail.com>
2022-12-07 23:10:06 +0700

    Update DiscoverViewModel.kt

--- app/src/main/java/com/imkryp70n/kryp70nnime/viewmodel/discover/DiscoverViewModel.kt
+++ app/src/main/java/com/imkryp70n/kryp70nnime/viewmodel/discover/DiscoverViewModel.kt
@@ -4,10 +4,13 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.imkryp70n.kryp70nnime.data.discovery.OCDiscover
+import com.imkryp70n.kryp70nnime.data.trending.OCTrending
 import com.imkryp70n.kryp70nnime.model.discovery.DiscoveryModel
 import com.imkryp70n.kryp70nnime.model.discovery.DiscoveryRepository
+import com.imkryp70n.kryp70nnime.model.trending.TrendingModel
+import com.imkryp70n.kryp70nnime.model.trending.TrendingRepository
 
-class DiscoverViewModel(private val repository: DiscoveryRepository) : ViewModel() {
+class DiscoverViewModel(private val repository: DiscoveryRepository , private val trendingRepository: TrendingRepository) : ViewModel() {
 
     private val _discoveries = MutableLiveData<DiscoveryModel>()
     val discoveries: LiveData<DiscoveryModel> = _discoveries
@@ -38,6 +41,36 @@ class DiscoverViewModel(private val repository: DiscoveryRepository) : ViewModel
                 } else {
                     _discoveries.value = callback
                 }
+            }
+        })
+    }
+
+
+    private val _getTrending = MutableLiveData<TrendingModel>()
+    val getTrending: LiveData<TrendingModel> = _getTrending
+
+    private val _isTrendingViewLoading = MutableLiveData<Boolean>()
+    val isViewLoadingTrending: LiveData<Boolean> = _isTrendingViewLoading
+
+    private val _onTrendingMessageError = MutableLiveData<Any?>()
+    val onTrendingMessageError: MutableLiveData<Any?> = _onTrendingMessageError
+
+    private val _isTrendingEmptyList = MutableLiveData<Boolean>()
+    val isTrendingEmptyList: LiveData<Boolean> = _isTrendingEmptyList
+
+    fun loadTrending(){
+        trendingRepository.fetchTrending(object : OCTrending<TrendingModel>{
+            override fun onSuccess(callback: TrendingModel) {
+                if (callback.results!!.isEmpty()){
+                    _isTrendingEmptyList.value = true
+                } else {
+                    _getTrending.value = callback
+                }
+            }
+
+            override fun onError(error: String?) {
+                _onTrendingMessageError.value = error
+                _isTrendingViewLoading.value = false
             }
         })
     }
