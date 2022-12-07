728840718bfc1a7ae5dd9a810b5c5360283a8086
kryp70n <joemusicamv12@gmail.com>
2022-12-07 23:10:13 +0700

    Create TrendingViewModel.kt

---
+++ app/src/main/java/com/imkryp70n/kryp70nnime/viewmodel/trending/TrendingViewModel.kt
@@ -0,0 +1,38 @@
+package com.imkryp70n.kryp70nnime.viewmodel.trending
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.imkryp70n.kryp70nnime.data.trending.OCTrending
+import com.imkryp70n.kryp70nnime.model.trending.TrendingModel
+import com.imkryp70n.kryp70nnime.model.trending.TrendingRepository
+
+class TrendingViewModel(private val repository: TrendingRepository) : ViewModel() {
+
+    private val _getTrending = MutableLiveData<TrendingModel>()
+    val getTrending: LiveData<TrendingModel> = _getTrending
+
+    private val _isViewLoading = MutableLiveData<Boolean>()
+    val isViewLoading: LiveData<Boolean> = _isViewLoading
+
+    private val _onMessageError = MutableLiveData<Any?>()
+    val onMessageError: MutableLiveData<Any?> = _onMessageError
+
+    private val _isEmptyList = MutableLiveData<Boolean>()
+    val isEmptyList: LiveData<Boolean> = _isEmptyList
+
+    fun getTrending() {
+        _isViewLoading.postValue(true)
+        repository.fetchTrending(object : OCTrending<TrendingModel>{
+            override fun onSuccess(callback: TrendingModel) {
+                _isViewLoading.postValue(false)
+                _getTrending.postValue(callback)
+            }
+
+            override fun onError(error: String?) {
+                _isViewLoading.postValue(false)
+                _onMessageError.postValue(error)
+            }
+        })
+    }
+}
