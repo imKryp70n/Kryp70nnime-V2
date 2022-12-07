6d91426cc1f81946eef138ad367c174cb445940f
kryp70n <joemusicamv12@gmail.com>
2022-12-07 23:09:57 +0700

    Update DiscoveryFragment.kt

--- app/src/main/java/com/imkryp70n/kryp70nnime/view/fragments/discover/DiscoveryFragment.kt
+++ app/src/main/java/com/imkryp70n/kryp70nnime/view/fragments/discover/DiscoveryFragment.kt
@@ -8,11 +8,14 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.imkryp70n.kryp70nnime.R
 import com.imkryp70n.kryp70nnime.di.Injection
 import com.imkryp70n.kryp70nnime.model.discovery.DiscoveryModel
+import com.imkryp70n.kryp70nnime.model.discovery.ResultsItem
+import com.imkryp70n.kryp70nnime.model.trending.TrendingModel
 import com.imkryp70n.kryp70nnime.viewmodel.discover.DiscoverViewModel
 
 class DiscoveryFragment : Fragment() {
@@ -24,8 +27,12 @@ class DiscoveryFragment : Fragment() {
     }
 
 
-    lateinit var adapter: DiscoverAdapter
+    lateinit var adapterDiscover: DiscoverAdapter
+    lateinit var adapterTrending : TrendingAdapter
+
+
     lateinit var rvAnimeList : RecyclerView
+    lateinit var tvAnimeTrending : RecyclerView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -39,6 +46,8 @@ class DiscoveryFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         setupViewModel()
         rvAnimeList = view.findViewById(R.id.rvAnimeList)
+        tvAnimeTrending = view.findViewById(R.id.rvAnimeTrendingList)
+
 
     }
 
@@ -47,18 +56,23 @@ class DiscoveryFragment : Fragment() {
         viewModel.isViewLoading.observe(viewLifecycleOwner, isViewLoadingObserver)
         viewModel.onMessageError.observe(viewLifecycleOwner, onMessageErrorObserver as Observer<in Any?>)
         viewModel.isEmptyList.observe(viewLifecycleOwner, emptyListObserver)
+
+        // Trending
+        viewModel.getTrending.observe(viewLifecycleOwner, renderTrending)
+        viewModel.isViewLoadingTrending.observe(viewLifecycleOwner, isViewLoadingTrendingObserver)
+        viewModel.onTrendingMessageError.observe(viewLifecycleOwner, onTrendingErrorObserver as Observer<in Any?>)
+        viewModel.isTrendingEmptyList.observe(viewLifecycleOwner, emptyListTrendingObserver)
     }
 
 
     //observers
     private val renderDiscover = Observer<DiscoveryModel>  {
         Log.d(TAG, "renderDiscover: $it")
-        adapter = DiscoverAdapter()
-        adapter.setData(it.results!!)
+        adapterDiscover = DiscoverAdapter(viewModel.discoveries.value!!.results!!)
         // horizontal
         rvAnimeList.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
-        rvAnimeList.adapter = adapter
-        adapter.update(it.results)
+        rvAnimeList.adapter = adapterDiscover
+        adapterDiscover.update(it.results!!)
     }
 
     private val isViewLoadingObserver = Observer<Boolean> {
@@ -74,9 +88,32 @@ class DiscoveryFragment : Fragment() {
     }
 
 
+
+    // Trending Observers
+    private val renderTrending = Observer<TrendingModel>  {
+        adapterTrending = TrendingAdapter(viewModel.getTrending.value!!.results!!)
+        // Horizontal
+        tvAnimeTrending.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+        tvAnimeTrending.adapter = adapterTrending
+        adapterTrending.update(it.results!!)
+    }
+    private val isViewLoadingTrendingObserver = Observer<Boolean> {
+        Log.d(TAG, "isViewLoadingObserver: $it")
+    }
+
+    private val onTrendingErrorObserver = Observer<Any> {
+        Log.d(TAG, "onMessageErrorObserver: $it")
+    }
+
+    private val emptyListTrendingObserver = Observer<Boolean> {
+        Log.d(TAG, "emptyListObserver: $it")
+    }
+
+
     override fun onResume() {
         super.onResume()
         viewModel.loadDiscover()
+        viewModel.loadTrending()
     }
 
     override fun onDestroy() {
