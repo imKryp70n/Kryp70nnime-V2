c96090eb63ef5a2e3c616f0c42b067460126f248
kryp70n <joemusicamv12@gmail.com>
2022-12-07 23:09:09 +0700

    Update MainActivity.kt

--- app/src/main/java/com/imkryp70n/kryp70nnime/view/Activity/MainActivity.kt
+++ app/src/main/java/com/imkryp70n/kryp70nnime/view/Activity/MainActivity.kt
@@ -25,7 +25,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setupView()
-
         bottomBarLogic()
     }
 
