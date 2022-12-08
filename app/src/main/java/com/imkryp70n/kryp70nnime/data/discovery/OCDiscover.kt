1951988e40e6f9264382ae58e6637291434041cd
kryp70n <joemusicamv12@gmail.com>
2022-12-08 23:13:24 +0700

    delete some oc

--- app/src/main/java/com/imkryp70n/kryp70nnime/data/discovery/OCDiscover.kt
+++
@@ -1,8 +0,0 @@
-package com.imkryp70n.kryp70nnime.data.discovery
-
-import com.imkryp70n.kryp70nnime.model.discovery.DiscoveryModel
-
-interface OCDiscover<T> {
-    fun onSuccess(callback: DiscoveryModel)
-    fun onError(error: String?)
-}
--- app/src/main/java/com/imkryp70n/kryp70nnime/data/trending/OCTrending.kt
+++
@@ -1,9 +0,0 @@
-package com.imkryp70n.kryp70nnime.data.trending
-
-import com.imkryp70n.kryp70nnime.model.discovery.DiscoveryModel
-import com.imkryp70n.kryp70nnime.model.trending.TrendingModel
-
-interface OCTrending <T> {
-    fun onSuccess(callback: TrendingModel)
-    fun onError(error: String?)
-}
