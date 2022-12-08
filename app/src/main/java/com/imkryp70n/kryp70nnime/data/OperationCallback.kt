7d73f3add73669456dc6079cebb71a729d99fee7
kryp70n <joemusicamv12@gmail.com>
2022-12-08 23:13:12 +0700

    Create OperationCallback.kt

---
+++ app/src/main/java/com/imkryp70n/kryp70nnime/data/OperationCallback.kt
@@ -0,0 +1,16 @@
+package com.imkryp70n.kryp70nnime.data
+
+import com.imkryp70n.kryp70nnime.model.discovery.DiscoveryModel
+import com.imkryp70n.kryp70nnime.model.trending.TrendingModel
+
+class OperationCallback {
+    interface OCDiscover<T> {
+        fun onSuccess(callback: DiscoveryModel)
+        fun onError(error: String?)
+    }
+
+    interface OCTrending <T> {
+        fun onSuccess(callback: TrendingModel)
+        fun onError(error: String?)
+    }
+}
